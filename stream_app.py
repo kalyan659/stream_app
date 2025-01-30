f2490a406d46fd07e0a776b755429792896c9765
Kalyanbrat Medhi <143828687+kalyan659@users.noreply.github.com>
2025-01-30 17:52:37 +0530

    first stream web app

---
+++ stream_app.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  4 16:53:08 2024
+
+@author: kalyanbrat
+"""
+
+import streamlit as st
+import requests
+
+# Title of the app
+st.title('My  App')
+
+# Display a text message
+st.write('Hello, welcome to My web app !')
+
